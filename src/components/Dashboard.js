e4018a74125cfeafd7c96cf96e632821382c9f7c
michaelslyusar <michael.slyusar@gmail.com>
2021-07-17 20:54:42 +0200

    dev_01

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const dashboard = () => {
+    return (
+
+        <div className="dashboardWrapper">
+            <h1>
+                Welcome to dashboard
+            </h1>
+        </div>
+
+    )
+}
+
+export default dashboard;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import ProtectedRoute from '../components/ProtectedRoute';
 
 import LoginPage from '../components/LoginPage';
+import Dashboard from '../components/Dashboard';
 import RegistrationPage from '../components/RegistrationPage';
 import NotFoundPage from '../components/NotFoundPage';
 
@@ -13,13 +14,15 @@ const AppRouter = () => (
             <Switch>
                 <Route path="/" component={LoginPage} exact={true} />
                 <Route path="/register" component={RegistrationPage} />
-                
+                <Route path="/dashboard" component={Dashboard} />
+
                 {/* {localStorage.getItem('token')? <Route path="/posts"><Posts /></Route>: null} */}
                 {/* <ProtectedRoute 
                     exact
                     path="/posts"
                     component={ Posts }
                 /> */}
+
                 <Redirect to="/" />
             </Switch>
         </BrowserRouter>
